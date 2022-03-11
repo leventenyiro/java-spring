8251ebe6950e2ec22b7f8ffc953f32e4e8490d9b
Levente Nyiro <lnyiro@berriesand.co>
2022-03-11 15:42:45 +0100

    bug fix in confirmationtoken

--- src/main/java/com/leventenyiro/javaspring/registration/token/ConfirmationToken.java
+++ src/main/java/com/leventenyiro/javaspring/registration/token/ConfirmationToken.java
@@ -3,9 +3,11 @@ package com.leventenyiro.javaspring.registration.token;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class ConfirmationToken {
+    @Id
     private String id;
     private String token;
     private LocalDateTime createdAt;
